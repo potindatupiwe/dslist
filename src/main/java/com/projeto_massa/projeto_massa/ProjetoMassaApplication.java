50bdf643e1738ea27d38f7ecc7c68e5d802e225d
potindatupiwe <appdjango298@gmail.com>
2025-07-24 21:29:19 -0300

    Project Creation

---
+++ src/main/java/com/projeto_massa/projeto_massa/ProjetoMassaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_massa.projeto_massa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMassaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMassaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto_massa/projeto_massa/ProjetoMassaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_massa.projeto_massa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMassaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
